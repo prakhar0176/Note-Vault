bec41aa849bac414f433fc2f1f88eb5ee8c7f624
prakhar-revemax <prakar.t@revemax.ai>
2024-07-16 20:38:32 +0530

    added react bootrap and navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,15 @@
+// import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header';
 
-function App() {
-  return (
-    <div className="App">
-      Welcome Prakhar
-    </div>
-  );
-}
+
+const App = () => (
+  <>
+    <Header />
+    <main style={{ minHeight: "93vh" }}></main>
+    <Footer />
+  </>
+)
 
 export default App;
